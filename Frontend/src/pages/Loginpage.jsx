0a026c68686ffce4555613ee35d7aa1dcbc22c98
keshvi1209 <keshviagarwal2004@gmail.com>
2025-06-24 11:40:27 +0530

    add: basic login page UI with form and styling

---
+++ Frontend/src/pages/Loginpage.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Login from "../components/Login";
+import myimage from "../assets/logo.svg"; // Importing the image
+import style from './Loginpage.module.css';
+
+function Loginpage() {
+  return (
+    <div className={style.loginpage}>
+
+    <div className={style.leftContainer}>
+    <Login />
+    </div>
+
+    <div className={style.rightContainer}>
+    <img 
+      src={myimage}
+      alt="Right Side" 
+      className={style.imageContainer}
+    />
+    </div>
+
+</div>
+
+  );
+}
+
+export default Loginpage;
