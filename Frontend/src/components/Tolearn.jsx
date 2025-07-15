ee7e3b30789ab6d4c91c8bc04c34f70c149f9628
keshvi agarwal <keshviagarwal2004@gmail.com>
2025-07-16 02:01:46 +0530

    feat(ui) toggling between two cards on skillentry page

---
+++ Frontend/src/components/Tolearn.jsx
@@ -0,0 +1,115 @@
+import React from 'react'
+import styles from "../pages/SkillsEntry.module.css";
+
+function Tolearn() {
+  return (
+    <form className={styles.canteachform}>
+      <div className={styles.rowContainer}>
+          <div className={styles.selectContainer}>
+            <label className={styles.inputhead}>Skill</label>
+            <input
+              type="text"
+              className={styles.input}
+              placeholder="Enter your skill"
+              name="skill"
+              // value={formData.skill}
+              // onChange={handleChange}
+            />
+          </div>
+
+         
+        </div>
+
+        <div className={styles.rowContainer}>
+          <div className={styles.selectContainer}>
+            <label className={styles.inputhead}>Current Proficiency Level</label>
+            <select
+              className={styles.select}
+              name="proficiency"
+              // value={formData.proficiency}
+              // onChange={handleChange}
+            >
+              <option value="null">Select</option>
+              <option value="beginner">Beginner</option>
+              <option value="intermediate">Intermediate</option>
+              <option value="advanced">Advanced</option>
+            </select>
+          </div>
+
+        
+        </div>
+
+        <div className={styles.rowContainer}>
+          <div className={styles.modeselectContainer}>
+            <label className={styles.inputhead}>Preferred Mode of Learning</label>
+            <select
+              className={styles.select}
+              name="mode"
+              // value={formData.mode}
+              // onChange={handleChange}
+            >
+              <option value="null">Select</option>
+              <option value="online">Online</option>
+              <option value="offline">Offline</option>
+              <option value="both">Both</option>
+            </select>
+          </div>
+
+          <div className={styles.languageselectContainer}>
+            <label className={styles.inputhead}>Preferred Language(s)</label>
+            <input
+              type="text"
+              className={styles.input}
+              placeholder="Languages you want to learn in"
+              name="languages"
+              // value={formData.languages}
+              // onChange={handleChange}
+            />
+          </div>
+        </div>
+
+        
+
+        <div className={styles.rowContainer}>
+          <div className={styles.yearelectContainer}>
+            <label className={styles.inputhead}>Tags</label>
+            <input
+              type="text"
+              className={styles.input}
+              placeholder="Add tags (e.g., #Python, #DataScience)"
+              name="tags"
+              // value={formData.tags}
+              // onChange={handleChange}
+            />
+          </div>
+        </div>
+
+        <div className={styles.rowContainer}>
+          <div className={styles.submitContainer}>
+            <button type="submit" className={styles.submitbutton}>
+              Submit
+            </button>
+          </div>
+
+          <div className={styles.skillcheckbuttonContainer}>
+            <button type="button" className={styles.custombutton}>
+              Added skills
+              {/* <span className={styles.badge}>4</span> */}
+            </button>
+          </div>
+        </div>
+
+        {/* {showAvailabilityPopup && (
+              <div style={{ textAlign: "center", marginTop: "10px" }}>
+                <p>[Availability Modal Placeholder]</p>
+                <button >
+                  Close
+                </button>
+              </div>
+            )} */}
+      
+    </form>
+  )
+}
+
+export default Tolearn
