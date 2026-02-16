fcfaf19408ddce6ae29b0136fa417c52cefdd74f
keshvi agarwal <keshviagarwal2004@gmail.com>
2026-02-16 15:12:26 +0530

    fixed authentication routing

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,1 +1,22 @@
-onfig.js
+// src/api.js
+import axios from "axios";
+import API_BASE_URL from "./config";
+
+const api = axios.create({
+  baseURL: API_BASE_URL,
+  withCredentials: true, 
+});
+
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+export default api;
